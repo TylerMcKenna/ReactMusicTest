de8dfe16fca809824955888980824b3067dd4cdb
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2024-10-30 09:43:04 -0500

    Got react project up

---
+++ client/music-app/src/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=&response_type=code&redirect_uri=http://localhost:3000"
+
+export default function Login() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
